ff923792e8e5a69db61530ed3778b8747466938e
pssharma1410 <piyushsharma141001@gmail.com>
2024-03-17 12:16:23 +0530

    Testing

--- Tray/src/main/java/com/example/tray/MainBottomSheet.kt
+++ Tray/src/main/java/com/example/tray/MainBottomSheet.kt
@@ -1075,9 +1075,9 @@ internal class MainBottomSheet : BottomSheetDialogFragment() {
             if (bottomSheetBehavior == null)
                 Log.d("bottomSheetBehavior is null", "check here")
 
-//            val screenHeight = requireContext().resources.displayMetrics.heightPixels
-//            val percentageOfScreenHeight = 0.7 // 90%
-//            val desiredHeight = (screenHeight * percentageOfScreenHeight).toInt()
+            val screenHeight = requireContext().resources.displayMetrics.heightPixels
+            val percentageOfScreenHeight = 0.7 // 70%
+            val desiredHeight = (screenHeight * percentageOfScreenHeight).toInt()
 
 //        // Adjust the height of the bottom sheet content view
 //        val layoutParams = bottomSheetContent.layoutParams
@@ -1085,7 +1085,7 @@ internal class MainBottomSheet : BottomSheetDialogFragment() {
 //        bottomSheetContent.layoutParams = layoutParams
             if(bottomSheetBehavior == null)
                 Log.d("MainBottomSheet  bottomSheet is null","Main Bottom Sheet")
-//            bottomSheetBehavior?.maxHeight = desiredHeight
+            bottomSheetBehavior?.maxHeight = desiredHeight
             bottomSheetBehavior?.isDraggable = false
             bottomSheetBehavior?.isHideable = false
 
