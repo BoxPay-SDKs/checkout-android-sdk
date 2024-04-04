58afa28bf18b77e5bf41a3dcde4cb140d4579c74
pssharma1410 <piyushsharma141001@gmail.com>
2024-04-04 17:32:09 +0530

    Compile SDK Reduced to level 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":Tray")))
     testImplementation("junit:junit:4.13.2")
