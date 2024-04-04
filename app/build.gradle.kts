fc566085a7fe3be8afcfaf6ab9bbb749a8abe75c
pssharma1410 <piyushsharma141001@gmail.com>
2024-04-04 16:58:34 +0530

    wallet and netbanking logos added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":Tray")))
     testImplementation("junit:junit:4.13.2")
