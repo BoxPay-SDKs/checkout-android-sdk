6f2805f06ed046fc73eb81e0f47087ed38604b7b
pssharma1410 <piyushsharma141001@gmail.com>
2024-04-04 16:44:28 +0530

    wallet and netbanking logos added

--- Tray/build.gradle.kts
+++ Tray/build.gradle.kts
@@ -43,12 +43,12 @@ android {
 
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.media3:media3-common:1.2.1")
+    implementation("androidx.media3:media3-common:1.1.0")
     implementation("com.google.android.gms:play-services-auth-api-phone:18.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
