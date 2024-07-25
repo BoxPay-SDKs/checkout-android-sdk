352d27e534c2cf293ec3291804a45ae8ad3a3a6e
Ishika1601-boxpay <ishika.bansal@boxpay.tech>
2024-07-25 15:32:59 +0530

    added jacoco

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
+    id("jacoco")
 }
 
 android {
@@ -19,10 +20,13 @@ android {
 
         buildConfigField("String", "SDK_VERSION", "\"1.1.10\"")
 
-        android.buildFeatures.buildConfig = true
+        buildFeatures.buildConfig = true
     }
 
     buildTypes {
+        debug {
+            isTestCoverageEnabled = true
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -31,20 +35,25 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures{
+
+    buildFeatures {
         viewBinding = true
     }
 }
+jacoco {
+    version = "0.8.7" // Set to the desired version
+}
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.9.0")
@@ -53,10 +62,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("com.android.volley:volley:1.2.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.android.volley:volley:1.2.1")
 }
-allprojects{
+
+allprojects {
     repositories {
         google()
         mavenCentral()
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.0-RC1" apply false
     id("com.android.library") version "8.4.1" apply false
 }
+
+buildscript {
+    dependencies {
+        classpath("org.jacoco:org.jacoco.core:0.8.7")// Add JaCoCo classpath here
+    }
+}
