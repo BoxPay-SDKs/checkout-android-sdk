1e1ed1b6b27f09a2dce5aa78c0cd88debd5caa47
Ishika1601-boxpay <ishika.bansal@boxpay.tech>
2024-09-09 14:53:18 +0530

    bugs solved

--- Sdk/src/main/java/com/boxpay/checkout/sdk/AddCardBottomSheet.kt
+++ Sdk/src/main/java/com/boxpay/checkout/sdk/AddCardBottomSheet.kt
@@ -795,6 +795,15 @@ internal class AddCardBottomSheet : BottomSheetDialogFragment() {
     }
 
     private fun isValidCardNumberByLuhn(stringInputCardNumber: String): Boolean {
+        // Define the minimum length for a valid card number
+        val minCardLength = 13
+
+        // Check if the card number meets the minimum length requirement
+        if (stringInputCardNumber.length < minCardLength) {
+            proceedButtonIsEnabled.value = false
+            return false
+        }
+
         var sum = 0
         var isSecondDigit = false
 
@@ -802,7 +811,7 @@ internal class AddCardBottomSheet : BottomSheetDialogFragment() {
             var d = stringInputCardNumber[i] - '0'
 
             if (isSecondDigit) {
-                d = d * 2
+                d *= 2
             }
 
             sum += d / 10
@@ -811,12 +820,11 @@ internal class AddCardBottomSheet : BottomSheetDialogFragment() {
             isSecondDigit = !isSecondDigit
         }
 
-        val result: Boolean = ((sum % 10) == 0)
+        val result: Boolean = (sum % 10 == 0)
 
-        if (!result)
+        if (!result) {
             proceedButtonIsEnabled.value = false
-
-
+        }
 
         return result
     }
