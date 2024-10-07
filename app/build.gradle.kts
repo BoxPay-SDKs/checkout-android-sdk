16abf18c8f5fa8ea3c7182872471d722f2a72ca8
Ishika1601-boxpay <ishika.bansal@boxpay.tech>
2024-10-07 14:44:03 +0530

    version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "SDK_VERSION", "\"1.1.18\"")
+        buildConfigField("String", "SDK_VERSION", "\"1.1.19\"")
 
         buildFeatures.buildConfig = true
     }
