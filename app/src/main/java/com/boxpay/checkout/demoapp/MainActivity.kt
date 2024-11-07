492a89555b4b1c308c7c58cbece01f8abf0906e1
ankushboxpay <ankush.kashyap@boxpay.tech>
2024-11-07 14:35:47 +0530

    UI bugs

--- app/src/main/java/com/boxpay/checkout/demoapp/MainActivity.kt
+++ app/src/main/java/com/boxpay/checkout/demoapp/MainActivity.kt
@@ -20,10 +20,6 @@ class MainActivity : AppCompatActivity() {
         val sharedPrefs = getSharedPreferences("TransactionDetails", Context.MODE_PRIVATE).edit()
         sharedPrefs.clear()
         sharedPrefs.apply()
-        val config = ClarityConfig("o4josf35jv")
-        Clarity.initialize(applicationContext, config)
-
-
         binding.openByDefault.setOnClickListener {
             //        var intent: Intent? = null
             try {
