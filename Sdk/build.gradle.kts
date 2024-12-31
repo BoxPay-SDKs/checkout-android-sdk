6bdb9f294788ada6e3772899af32eef2c8873aa2
Ishika1601-boxpay <ishika.bansal@boxpay.tech>
2024-12-31 09:36:04 +0530

    version update

--- Sdk/build.gradle.kts
+++ Sdk/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
 
-        buildConfigField("String", "SDK_VERSION", "\"1.1.21-beta1\"")
+        buildConfigField("String", "SDK_VERSION", "\"1.1.22-beta1\"")
 
         android.buildFeatures.buildConfig = true
     }
