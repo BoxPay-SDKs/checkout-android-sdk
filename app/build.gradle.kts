55de6b5ecbda9e64bca461ee1cd732dda7300405
Ishika1601-boxpay <ishika.bansal@boxpay.tech>
2025-02-24 10:53:05 +0530

    ui event fix added- removed message in event key and added in extra attributes with key message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "SDK_VERSION", "\"1.2.2-beta4\"")
+        buildConfigField("String", "SDK_VERSION", "\"1.2.2-beta5\"")
 
         buildFeatures.buildConfig = true
     }
