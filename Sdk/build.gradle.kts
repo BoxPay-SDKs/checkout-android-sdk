3c7b618e551bffa118e5e1672c37f27574d3ba43
Jenkins CI <ci@boxpay.tech>
2025-03-28 12:28:53 +0000

    Update SDK version to 1.2.6 [Production]

--- Sdk/build.gradle.kts
+++ Sdk/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
 
-        buildConfigField("String", "SDK_VERSION", "\"1.2.6-beta1\"")
+        buildConfigField("String", "SDK_VERSION", "\"1.2.6\"")
 
         android.buildFeatures.buildConfig = true
     }
