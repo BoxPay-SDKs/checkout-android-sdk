b7aabcd8fed292e55247967dbb4a6225ba06318a
ishika bansal <ishika.bansal@boxpay.tech>
2025-09-12 15:30:38 +0530

    added signing config in build gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,22 @@ android {
         abortOnError = false
     }
 
+    signingConfigs {
+        create("release") {
+            val keystorePath: String? = System.getenv("KEYSTORE_PATH")
+            val storePassword: String? = System.getenv("STORE_PASSWORD")
+            val keyAlias: String? = System.getenv("KEY_ALIAS")
+            val keyPassword: String? = System.getenv("KEY_PASSWORD")
+
+            if (keystorePath != null) {
+                storeFile = file(keystorePath)
+                storePassword?.let { this.storePassword = it }
+                keyAlias?.let { this.keyAlias = it }
+                keyPassword?.let { this.keyPassword = it }
+            }
+        }
+    }
+
     buildTypes {
         debug {
             isTestCoverageEnabled = true
