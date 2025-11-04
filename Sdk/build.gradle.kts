30911827b8f3c12d6d8f2a734b126b51178363a4
Jenkins CI <ci@boxpay.tech>
2025-11-04 07:38:52 +0000

    Update SDK version to 1.2.12-beta1

--- Sdk/build.gradle.kts
+++ Sdk/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
 
-        buildConfigField("String", "SDK_VERSION", "\"1.2.11\"")
+        buildConfigField("String", "SDK_VERSION", "\"1.2.12-beta1\"")
 
         android.buildFeatures.buildConfig = true
     }
