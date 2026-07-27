a14149d246184505f5e3546900006b7fc4c9267e
ishika bansal <ishika.bansal@boxpay.tech>
2026-07-27 16:42:25 +0530

    updated the version of cross platform sdk

--- Sdk/build.gradle.kts
+++ Sdk/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.boxpay.checkout.sdk"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 21
@@ -71,7 +71,7 @@ dependencies {
     androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
     testImplementation("org.mockito:mockito-core:5.7.0")
     implementation("androidx.activity:activity-compose:1.5.1")
-    api("com.github.BoxPay-SDKs.cross-platform-sdk:cross-platform-sdk:1.0.2-beta4")
+    api("com.github.BoxPay-SDKs.cross-platform-sdk:cross-platform-sdk:1.0.2-beta5")
 }
 
 
